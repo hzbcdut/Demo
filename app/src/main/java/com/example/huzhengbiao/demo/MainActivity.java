3c8a835bbfef167ef583e4814e89e864f101c6df
huzhengbiao <hzhengbiao@126.com>
2018-10-17 11:21:46 +0800

    动画的启动和停止时间。

--- app/src/main/java/com/example/huzhengbiao/demo/MainActivity.java
+++ app/src/main/java/com/example/huzhengbiao/demo/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.huzhengbiao.demo;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final Object TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
         anim.setRepeatMode(Animation.REVERSE);
         anim.setRepeatCount(Animation.INFINITE);
         tv.startAnimation(anim);
+
+        View root = findViewById(R.id.root);
+        root.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+            @Override
+            public void onViewAttachedToWindow(View v) {
+                Log.d("debug", TAG + "-->onViewAttachedToWindow(View v)");
+            }
+
+            @Override
+            public void onViewDetachedFromWindow(View v) {
+                Log.d("debug", TAG + "-->onViewDetachedFromWindow(View v)");
+            }
+        });
+        // View 什么关联到窗口   自定义View的onDetachFromWindow什么时候会被调用。
+
+        // 开始动画 和取消动画  onActivity的onStart开始动画， 在onStop取消动画
     }
 }
