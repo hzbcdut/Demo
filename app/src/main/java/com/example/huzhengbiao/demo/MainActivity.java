b9c7ea6a9306a7d46cb9f504a6aaeac4e8c23b42
huzhengbiao <hzhengbiao@126.com>
2018-10-17 11:07:48 +0800

    让View闪烁的补间动画 （也可以用属性动画实现一样的） 帧动画来可以实现图片闪烁的效果

--- app/src/main/java/com/example/huzhengbiao/demo/MainActivity.java
+++ app/src/main/java/com/example/huzhengbiao/demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.huzhengbiao.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.tv);
+
+        Animation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(500); //You can manage the blinking time with this parameter
+        anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        tv.startAnimation(anim);
     }
 }
