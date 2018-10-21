2df3ef045b96756f0adfd772b093839107c4de4a
huzhengbiao <hzhengbiao@126.com>
2018-10-21 15:48:54 +0800

    获取虚拟按键的高度 隐藏虚拟按键

--- navigationbardemo/src/main/java/com/example/navigationbardemo/MainActivity.java
+++ navigationbardemo/src/main/java/com/example/navigationbardemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.navigationbardemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       int navigationBarHeight =  NavigationBarUtil.getNavigationBarHeight(this);
+        Log.d("debug", " --> navigationBarHeight = " + navigationBarHeight);
+
+        NavigationBarUtil.hideBottomUIMenu(this);
     }
 }
